001ad0f12134c637fd5cec737c6d71a7cfb06fb6
Oluwole Olusegun Joshua <oluwoleolusegun14@gmail.com>
2025-02-14 09:40:29 +0100

    Update books.py

--- api/routes/books.py
+++ api/routes/books.py
@@ -62,7 +62,7 @@ async def delete_book(book_id: int) -> None:
     return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content=None)
 
 @router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
-async def get_book(book_id: int) -> Book
+async def get_book(book_id: int) -> Book:  errgha
     book = db.get_book(book_id)
     if not book:
         return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={"detail": "Book not found"})
